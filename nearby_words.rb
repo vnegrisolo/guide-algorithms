eb8f17e34134ea9d8d574cd1e6b9b86ac5b7610d
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2015-01-11 12:30:21 -0200

    new algorithm for nearby words

---
+++ nearby_words.rb
@@ -0,0 +1,43 @@
+require 'pry'
+
+# Algorith to suggesting when typing
+class NearbyWords
+  DICTIONARY = %w(hi go)
+
+  # Usage NearbyWords.new.nearby_words('hi')
+  def nearby_words(word)
+    recursive_nearby_words(word)
+  end
+
+  private
+
+  def recursive_nearby_words(word, result = [], partial = '')
+    if partial.length >= word.length
+      result << partial if word?(partial)
+      return result
+    end
+
+    nearby_chars(word[partial.length]).each do |c|
+      recursive_nearby_words(word, result, add_char(partial, c))
+    end
+    result
+  end
+
+  def add_char(word, char)
+    new_word = String.new(word)
+    new_word << char
+    new_word
+  end
+
+  def nearby_chars(char)
+    case char
+    when 'h' then %w(h g j y b n)
+    when 'i' then %w(i u o j k)
+    else []
+    end
+  end
+
+  def word?(word)
+    DICTIONARY.include? word
+  end
+end
