ac091eb4390e2e77666b1d48027229e9ca6a87d2
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2015-01-11 12:31:35 -0200

    new algorithm for palindrome

---
+++ spec/palindrome_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe Palindrome do
+  subject { described_class.new }
+
+  describe '#palindrome?' do
+    context 'is palindrome' do
+      %w(
+        'a'
+        '5'
+        'a6a'
+        '5g5'
+        'asdf98766789fdsa'
+      ).each do |t|
+        it { expect(subject.palindrome?(t)).to be true }
+      end
+    end
+
+    context 'is not palindrome' do
+      %w(
+        'a6b'
+        '9g5'
+        'asd87667fdsa'
+      ).each do |t|
+        it { expect(subject.palindrome?(t)).to be false }
+      end
+    end
+  end
+
+  describe '#let_alpha_numeric' do
+    it 'success' do
+      expect(subject.send(:let_alpha_numeric, ' 3 d=a z 0 9')).to eq('3daz09')
+    end
+  end
+end
